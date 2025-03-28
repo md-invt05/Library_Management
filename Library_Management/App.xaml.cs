4c15fccf3043b7414bc8cc3fa47a2a4ed2d8b74f
Maxim <maks.degtearyov25@gmail.com>
2025-03-29 01:00:56 +0300

    add Configuration file

--- Library_Management/App.xaml.cs
+++ Library_Management/App.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
 
 namespace Library_Management;
 
@@ -9,4 +11,15 @@ namespace Library_Management;
 /// </summary>
 public partial class App : Application
 {
+    public static class ConfigurationHelper
+    {
+        public static IConfigurationRoot GetConfiguration()
+        {
+            // Создаём билдер конфигурации
+            return new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory) // или Directory.GetCurrentDirectory()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+        }
+    }
 }
--- Library_Management/Data/LibraryDbContext.cs
+++ Library_Management/Data/LibraryDbContext.cs
@@ -1,6 +1,7 @@
 using Library_Management.Models;
 using Microsoft.EntityFrameworkCore;
 using System.IO;
+using Microsoft.Extensions.Configuration;
 
 namespace Library_Management.Data;
 
@@ -14,41 +15,47 @@ public class LibraryDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        string dbPath = Path.Combine(Directory.GetCurrentDirectory(), "library.db");
-        optionsBuilder.UseNpgsql($"Data Source={dbPath}");
+        if (!optionsBuilder.IsConfigured)
+        {
+            // Получаем конфигурацию
+            var configuration = App.ConfigurationHelper.GetConfiguration();
+            // Считываем строку подключения
+            var connectionString = configuration.GetConnectionString("LibraryDb");
+
+            // Подключаемся к PostgreSQL
+            optionsBuilder.UseNpgsql(connectionString);
+        }
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Book>(entiy =>
-            {
-                entiy.HasKey(b => b.Id);
-                entiy.Property(b => b.Title).IsRequired().HasMaxLength(200);
-                entiy.Property(b => b.ISBN).IsRequired().HasMaxLength(20);
-                entiy.Property(b => b.PublishYear).IsRequired();
-                entiy.Property(b => b.QuantitiyInStock).IsRequired();
-                
-                entiy.HasOne(b => b.Author) 
-                     .WithMany(a => a.Books)
-                     .HasForeignKey(b => b.AuthorId)
-                     .OnDelete(DeleteBehavior.Cascade);
-                
-                entiy.HasOne(b => b.Genre)
-                    .WithMany(g => g.Books)
-                    .HasForeignKey(b => b.GenreId)
-                    .OnDelete(DeleteBehavior.Cascade);
-            }
-        );
+        {
+            entiy.HasKey(b => b.Id);
+            entiy.Property(b => b.Title).IsRequired().HasMaxLength(200);
+            entiy.Property(b => b.ISBN).IsRequired().HasMaxLength(20);
+            entiy.Property(b => b.PublishYear).IsRequired();
+            entiy.Property(b => b.QuantitiyInStock).IsRequired();
+            
+            entiy.HasOne(b => b.Author) 
+                 .WithMany(a => a.Books)
+                 .HasForeignKey(b => b.AuthorId)
+                 .OnDelete(DeleteBehavior.Cascade);
+            
+            entiy.HasOne(b => b.Genre)
+                .WithMany(g => g.Books)
+                .HasForeignKey(b => b.GenreId)
+                .OnDelete(DeleteBehavior.Cascade);
+        });
 
         modelBuilder.Entity<Author>(entity =>
-            {
-                entity.HasKey(a => a.Id);
-                entity.Property(a => a.FirstName).IsRequired().HasMaxLength(100);
-                entity.Property(a => a.LastName).IsRequired().HasMaxLength(100);
-                entity.Property(a => a.DateBirth).IsRequired();
-                entity.Property(a => a.Country).IsRequired().HasMaxLength(100);
-            }
-        );
+        {
+            entity.HasKey(a => a.Id);
+            entity.Property(a => a.FirstName).IsRequired().HasMaxLength(100);
+            entity.Property(a => a.LastName).IsRequired().HasMaxLength(100);
+            entity.Property(a => a.DateBirth).IsRequired();
+            entity.Property(a => a.Country).IsRequired().HasMaxLength(100);
+        });
 
         modelBuilder.Entity<Genre>(entity =>
         {
